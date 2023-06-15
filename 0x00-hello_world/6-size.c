bc12b3c49f57901f07a0c8227fd838343dc0e3ef
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-06-15 14:51:16 -0700

    data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints exactly with proper grammar,
+ * but the outcome is a piece of art,
+ * followed by a new line.'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %2d byte(s) \n", sizeof(char));
+printf("Size of an int: %2d byte(s) \n", sizeof(int));
+printf("Size of an long int: %2d byte(s) \n", sizeof(long int));
+printf("Size of a long long int: %2d byte(s) \n", sizeof(long long int));
+printf("Size of a float: %2d byte(s) \n", sizeof(float));
+return (0);
+}
