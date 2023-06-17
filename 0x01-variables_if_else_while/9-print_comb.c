7934034a85f8d7e088db1b6898e266d05ead976c
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-06-16 19:23:06 -0700

    comma separated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'This program will assign a random number
+ * to the variable n each time it is executed.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 48 ; n <= 57 ; n++)
+{
+	putchar(n);
+	if (n != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	else if (n == 57)
+		putchar('\n');
+}
+return (0);
+}
