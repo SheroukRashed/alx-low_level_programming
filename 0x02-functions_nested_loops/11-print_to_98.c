2b0972c18fcdfa1e9190e5ecb0375f3c23fd81c3
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-06-20 18:49:41 -0700

    fix

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_to_98 - Entry point
+ *
+ * Description: 'This program will print alphabet'
+ * @n: int n
+ *
+ * Return: Void (Success)
+ */
+void print_to_98(int n)
+{
+	int k = n;
+
+	while (n <= 98)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+		n++;
+	}
+	while (k > 98)
+	{
+		if (k == 0)
+		{
+			printf("%d", k);
+		}
+		else
+		{
+			printf("%d, ", k);
+		}
+		k--;
+	}
+	putchar('\n');
+}
