a591d3154f2a19110aa91ca6f47e25e162ee833c
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-06-26 18:49:58 -0700

    Q9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - Entry point
+ *
+ * Description: 'This program will reverse a string
+ *
+ * @dest: Pointer to char to be tested
+ * @src: Pointer to char to be tested
+ *
+ * Return: Void (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int c = strlen(src) - 1;
+	while ( c >= 0 )
+	{
+		dest[c] = src[c];
+		c--;
+	}
+	return (dest);
+}
