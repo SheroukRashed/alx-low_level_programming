35f6f99f096110b67993ebd1b486d84b5725187a
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-06-30 16:20:00 -0700

    G2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,39 +12,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int total = n <= _strlen(src) ? n : _strlen(src);
+	int i;
 
-	while (*src != '\0' && n > 0)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-	*dest = '\0';
+	for (; n > i; i++)
+		dest[i] = '\0';
 
-	return (dest - total);
-}
-
-/**
- * _strlen - Entry point
- *
- * Description: 'This program will calculate string length
- *
- * @s: Pointer to char to be tested
- *
- * Return: Void (Success)
- */
-int _strlen(char *s)
-{
-	int count = 0;
-
-	while (*s != '\0')
-	{
-		count++;
-		s++;
-	}
-
-	return (count);
+	return (dest);
 }
