397a56672929be8b5e5a12c184619405be3b2023
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-07-04 16:38:18 -0700

    Q5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -41,9 +41,9 @@ int compare(char *str1, char *str2)
 {
 	unsigned int i;
 
-	for (i = 0 ; (str1[i] != '\0' && str2[i] != '\0') ; i++)
+	for (i = 0 ;  str2[i] != '\0' ; i++)
 	{
-		if (str1[i] != str2[i])
+		if (str1[i] == '\0' || str1[i] != str2[i])
 			return (0);
 	}
 	return (1);
