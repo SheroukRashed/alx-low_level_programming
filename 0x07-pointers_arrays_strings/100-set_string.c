27dcb0beded64577dd9e0583ee00e5e75e10bc4a
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-07-04 17:33:21 -0700

    .

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * set_string - Entry point
+ *
+ * Description: 'This program will set string
+ *
+ * @s: Char** to be tested
+ * @to: Char* to be tested
+ *
+ * Return: Void (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
