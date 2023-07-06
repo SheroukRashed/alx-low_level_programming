3b2426959080916fca04c38121d24c0f71952f98
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-07-05 19:30:37 -0700

    Q100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Entry point
+ *
+ * Description: 'This program will print string using _putchar
+ *
+ * @s: Char* to be tested
+ *
+ * Return: Integer (Success)
+ */
+int _strlen_recursion(char *s)
+{
+        int n = 0;
+
+        if (*s != '\0')
+        {
+                n = _strlen_recursion(s + 1) + 1;
+                return (n);
+        }
+        else
+                return (0);
+}
+#include "main.h"
+/**
+ * is_palindrome - Entry point
+ *
+ * Description: 'This program will print factorial n
+ *
+ * @s: char* to be tested
+ *
+ * Return: Integer (Success)
+ */
+int is_palindrome(char *s)
+{
+	return (_is_equal(s, 0, _strlen_recursion(s) - 1 ));
+}
+
+/**
+ * _is_equal - Entry point
+ *
+ * Description: 'This program will print factorial n
+ *
+ * @s: Char* to be tested
+ * @x: Int to be tested
+ * @y: Int to be tested
+ *
+ * Return: Integer (Success)
+ */
+int _is_equal(char *s, int x, int y)
+{
+	if (s[x] != s[y])
+		return (0);
+	if (x == y || y == x + 1)
+		return (1);
+	return (_is_equal(s, x + 1, y - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,6 @@ int _get_root(int x, int y);
 int _sqrt_recursion(int n);
 int _get_prime(int x, int y);
 int is_prime_number(int n);
+int _is_equal(char *s, int x, int y);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
