be93125b414f93597dbf541215c97888390ea72e
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-07-05 19:34:29 -0700

    Q100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,15 +10,15 @@
  */
 int _strlen_recursion(char *s)
 {
-        int n = 0;
+	int n = 0;
 
-        if (*s != '\0')
-        {
-                n = _strlen_recursion(s + 1) + 1;
-                return (n);
-        }
-        else
-                return (0);
+	if (*s != '\0')
+	{
+		n = _strlen_recursion(s + 1) + 1;
+		return (n);
+	}
+	else
+		return (0);
 }
 #include "main.h"
 /**
@@ -32,7 +32,7 @@ int _strlen_recursion(char *s)
  */
 int is_palindrome(char *s)
 {
-	return (_is_equal(s, 0, _strlen_recursion(s) - 1 ));
+	return (_is_equal(s, 0, _strlen_recursion(s) - 1));
 }
 
 /**
