d83cc02ef1886c711eb70caa6cf3827f8456163b
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-07-05 17:08:44 -0700

    Q2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * _puts_recursion - Entry point
+ * _strlen_recursion - Entry point
  *
  * Description: 'This program will print string using _putchar
  *
  * @s: Char* to be tested
  *
- * Return: Void (Success)
+ * Return: Integer (Success)
  */
-void _puts_recursion(char *s)
+int _strlen_recursion(char *s)
 {
+	int n = 0;
+
 	if (*s != '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		n = _strlen_recursion(s + 1) + 1;
+		return (n);
 	}
-	else 
-		_putchar('\n');
+	else
+		return (0);
 }
