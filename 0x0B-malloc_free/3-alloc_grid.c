698ead18916216f53941a05f77d2bc326663f6fb
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-07-12 19:37:22 -0700

    Q3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * alloc_grid - Entry point
+ *
+ * Description: 'This program will concate 2 strings
+ *
+ * @width: Int to be tested
+ * @height: Int to be tested
+ *
+ * Return: Char* (Success)
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (--i; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	return (arr);
+}
