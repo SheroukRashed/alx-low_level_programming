c32decc9581f7ad194974d1649b1ec45dc751dc8
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-07-12 18:11:28 -0700

    Q2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	size1 = strlen(s1);
 	size2 = strlen(s2);
