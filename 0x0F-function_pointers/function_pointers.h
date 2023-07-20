d5104734faac7510fe59a934cba799509779883a
SheroukRashed <sherouk.mohammed.rashed@gmail.com>
2023-07-19 17:11:55 -0700

    Fix Q0,1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
-
-/**
- * size_t - typedef for unsigned int
- */
-typedef unsigned int size_t;
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
